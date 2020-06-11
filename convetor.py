2ae6e2264ec108b019c3347dd948b6fd3f4c97f0
i-rinak <i-rinka@outlook.com>
2020-06-11 09:03:06 +0800

    20.6.11

--- convetor.py
+++
@@ -1,4 +0,0 @@
-import Util.FileProcessor as FP
-
-test_path = "Two\\data\\1998-01-2003版-带音.txt"
-FP.code_convert(test_path,"gbk","utf-8")
--- word_embindingtest.py
+++
@@ -1,6 +0,0 @@
-import torch
-bed = torch.zeros(20, 10)  # 高/长
-one = torch.ones(10)
-print(one[1])
-for i in one:
-    print(i)
